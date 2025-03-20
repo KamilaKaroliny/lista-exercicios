4d06a0d5c34065c6e7abb1dadcdb46100a82ae5b
Kamila Karoliny dos Santos <kamila_k_santos@estudante.sesisenai.org.br>
2025-03-20 10:07:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+//variavel_01
+//let num = prompt ('Digite um número')
+
+//atv 1
+/*function verificPar(){
+    if (num % 2 === 0 ){
+        console.log(num + ' é um número é par');
+    } else{
+        console.log( num +' é um número é ímpar');
+    }
+}
+
+verificPar(num);
+//fim atv 1
+
+
+
+//variavel_02
+let num2 = parseFloat (prompt('Digite o primeiro número'))
+let num3 = parseFloat (prompt('Digite o segundo número'))
+
+//atv 2
+function matSimples(num2,num3){
+    const soma = num2 + num3
+    const subtracao = num2 - num3
+    const multiplicacao = num2 * num3
+    console.log('A somas dos número é = ' + soma);
+    console.log('A subtração dos número é = ' + subtracao);
+    console.log('A multiplicação dos número é = ' + multiplicacao);
+    if (num2 === 0){
+        console.log('Não da pra dividir por 0')
+    } else {
+        const divisao = num2 / num3
+        console.log('A divisão dos número é = ' + divisao);
+    }
+}
+
+matSimples(num2,num3);
+*/
+
+//fim atv 2
+
+//variavel_03
+
+for (let i = 0; i < 10; i++) {
+   console.log ('número ' + i);
+} 
+
